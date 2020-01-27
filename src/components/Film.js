2a9e967e60451e62471c23240bb7b2dd116e3d38
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-27 15:55:58 +0000

    adds Film component

---
+++ src/components/Film.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Film extends Component {
+  render() {
+    return(
+      <p>I am a film</p>
+    )
+  }
+}
+
+export default Film;
--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
+import Film from './Film'
 
 class FilmList extends Component {
   render() {
     return(
-      <p>Film</p>
+      <>
+      <p>FilmList</p>
+      <Film/>
+      </>
     )
   }
 }
