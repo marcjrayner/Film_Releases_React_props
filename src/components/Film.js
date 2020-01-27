b3312d7395ddcb61cbf7a9d5ba1cc6ee67ca24d3
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-27 16:21:04 +0000

    MVP done

--- src/components/Film.js
+++ src/components/Film.js
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 class Film extends Component {
   render() {
     return(
-      <p>I am a film</p>
+      <div className="film">
+        <a href={this.props.url}>{this.props.name}</a>
+      </div>    
     )
   }
 }
